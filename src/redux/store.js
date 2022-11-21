4cdbd697695dc162553f9bc8d7fd514c6961fcc3
Andres Martinez <guiiral97@gmail.com>
2022-11-21 01:15:44 -0500

    MT-01-37

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducers/rootReducer";
 
 export const store = configureStore({
-    reducer: {},
+    reducer: rootReducer,
 });
